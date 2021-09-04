1566d98ebaea325251f86e018b92cb737fab28f9
1shaked <shaked1hen@gmail.com>
2021-09-04 13:01:41 +0300

    added the change to be compatible with html file

--- app.py
+++ app.py
@@ -1,42 +1,22 @@
 from flask import Flask, request
-from convertor import HtmlConverter, handleFlashes
+from convertor import HtmlConverter
 from flask_cors import CORS, cross_origin
 # import os
-import requests as r
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route('/<int:post_id>')
+@app.route('/<str:html_content>')
 @cross_origin()
-def index(post_id):
-    print(post_id)
+def index(html_content):
+    print(html_content)
     try:
         html = HtmlConverter()
-        html.initWithArticle(int(post_id))
-        html.soupToJson()
-        return {"success": "Successfully got article.", "data": html.respond }
+        html.initWithHtml(html_content)
+        
+        return {"success": "Successfully got render func.", "data": html.soupToJson() }
     except Exception as e:
         print(e)
         return {"error": "no article in this id" }
-
-import json
-@app.route('/api/')
-@cross_origin()
-def api_get():
-    path_args = request.args.get('path')
-    try:
-        BASE_URL: str= 'https://www.inn.co.il/'
-        res = r.get(BASE_URL + path_args)
-        is_flash = 'Generic/NewApi/Cat?type=10'.lower() in path_args.lower()
-        if is_flash:
-            respond = handleFlashes(res.json())
-            return respond
-        return res.json()
-    except Exception as e:
-        print(e)
-        return {"error": "no article in this id" }
-
-
 
 
 if __name__ == '__main__':
--- convertor.py
+++ convertor.py
@@ -1,4 +1,3 @@
-import requests
 from bs4 import BeautifulSoup, element
 import json
 from json import loads
@@ -26,28 +25,12 @@ class HtmlConverter:
     def __init__(self):
         pass
         
-    def initWithFlash(self, flash: str):
+    def initWithHtml(self, flash: str):
         self.parseHtmlContent(flash)
-        
-    def initWithArticle(self, articleId: int):
-        self.articleId = articleId
-        self.html :str= ''
-        self.json :dict = {}
-        self.respond: dict= {}
-
-        self.getHtmlContent()
-        self.parseHtmlContent()
-    
-    def getHtmlContent(self):
-        url :str= f'{BASE_URL}{self.articleId}'
-        self.respond :dict= requests.get(url).json()
 
     def parseHtmlContent(self, content = None, ads = True):
-        if content:
-            soup :BeautifulSoup = BeautifulSoup(content, "html.parser")
-        else:
-            soup :BeautifulSoup = BeautifulSoup(self.respond[CONTENT], "html.parser")
-        tags_to_delete :list[str] = ['head', 'style','script', 'noscript', 'link']
+        soup :BeautifulSoup = BeautifulSoup(content, "html.parser")
+        tags_to_delete :list[str] = ['head', 'style','script', 'noscript', 'link'] # here are list of tags you want to delete
         # deleting the tags
         for tag in tags_to_delete:
             els = soup.select(tag)
@@ -57,80 +40,10 @@ class HtmlConverter:
         elements_soup: list = soup.find_all(lambda tag: len(tag.attrs) > 0)
         for tag in elements_soup:
             # region a tag
-            if tag.name == 'a':
+            if tag.name == 'a': # exaple of auto rename tag to components
                 if 'https://twitter.com' in tag.attrs['href']:
                     tag.name = 'twitter-component'
             # endregion 
-            # region audio tag
-            elif tag.name == 'audio':
-                print(tag.attrs[DATA_PLAYER]) # {"type":3,"id":2088396,"title":"סנונית ראשונה? עו\\"ד פאלק","url":"a7radio/misc/audio/21/jan/falk27-1.mp3","image":0,"author":"","date":"2021-01-27T15:00:48","credit":"צילום: ערוץ 7"} 
-                data_player = loads(tag.attrs[DATA_PLAYER])
-                tag.name = 'audio-component'
-                required_attrs = ['id', 'title', 'credit']
-                for attrs in required_attrs:
-                    if attrs in data_player:
-                        tag.attrs[attrs] = data_player[attrs]
-            # endregion
-            # region ifrem
-            elif tag.name == IFRAME:
-                if 'youtube' in tag.attrs[SRC]:
-                    id_youtube = tag.attrs[SRC].split('/')[-1]
-                    tag.name = youtube_component
-                    tag.attrs['vid'] = id_youtube
-                    del tag.attrs['src']
-                    print(tag.attrs, tag.attrs.keys())
-                    
-                elif "googletagmanager" in tag.attrs[SRC]:
-                    tag.extract()
-            # endregion
-            # region videos tag
-            elif tag.name == 'div' and 'class' in tag.attrs and 'a7video' in tag.attrs['class'] and 'IVideo' in tag.attrs['class']:
-                data_player = tag.attrs[DATA_PLAYER]
-                print(data_player)
-                for img in tag:
-                    if type(img) == element.Tag:
-                        if 'video' in img.attrs['class']:
-                            tag['poster'] = img.attrs['src']
-
-                    img.extract()
-                if 'mp4' in tag.attrs['data-src']:
-                    tag.name = 'video-component'
-                    data_player = loads(tag.attrs[DATA_PLAYER])
-                    # fields: list = ['credit', 'date', 'title', 'author']
-                    tag.attrs['credit'] = data_player['credit']
-                    tag.attrs['alt'] = data_player['title']
-                else:
-                    tag.name = youtube_component
-                    if tag.attrs['data-src']:
-                        id_youtube = tag.attrs['data-src'].split('/')[-1]
-                        tag.attrs["vid"] = id_youtube
-                        del tag.attrs['src']
-                try:
-                    tag.parent.next_sibling.next_sibling.extract()
-                except Exception as e:
-                    print(e)
-            # endregion
-            # region img tag
-            elif tag.name == 'img':
-                if ('style' in tag.attrs):
-                    if 'display: none' in tag.attrs['style']:
-                        tag.extract()
-                        continue
-                if 'class' in tag.attrs:
-                    if ('video' in tag.attrs['class']) or ('play' in tag.attrs['class']):
-                        tag.extract()
-                        continue
-                
-                img_details = tag.next_sibling
-                tag.name = 'img-component'
-                if img_details != None:
-                    credit_div = img_details.find("div", {"class": "ImageCredit"})
-                    tag.attrs[SRC] = tag.attrs[SRC].split('/')[-1].split('.')[0] # get the id from 'path/to/id.png' => 'id.png' => id call flow
-                    if 'class' in img_details.attrs and 'ImageDesc' in img_details.attrs['class']:
-                        tag.attrs["alt"] = self.getImggeAlt(img_details) # str(img_details.contents[0]) or str(img_details.text) # credit
-                        tag.attrs["credit"] = credit_div.text.replace('\n', '') or img_details.text # credit
-                        img_details.extract()
-            # endregion
             for tag_to_remove in REMOVE_ATTRIBUTES:
                 if tag_to_remove in tag.attrs:
                     if tag.name in RESERVED_PROPERTIES and tag_to_remove in RESERVED_PROPERTIES[tag.name]:
@@ -138,8 +51,6 @@ class HtmlConverter:
                     del tag[tag_to_remove]
             # tag.attrs = [(key,value) for key,value in tag.attrs if key not in REMOVE_ATTRIBUTES]
         self.soup :BeautifulSoup = soup
-        if ads:
-            self.addAds()
         if soup.body is not None:
             self.html = soup.body.prettify(formatter='html')
             self.html = self.html.replace('\n', '').replace('<!DOCTYPE html>', '')
@@ -147,33 +58,11 @@ class HtmlConverter:
             self.html = str(soup)
 
     def soupToJson(self):
-        self.respond['Content2'] = pushToArray(self.soup, [])[0][CONTENT][0][CONTENT][0][CONTENT] # TODO: remove nesting so many objects (there is the default XXX and body and then main div)
+        return pushToArray(self.soup, [])[0][CONTENT][0][CONTENT][0][CONTENT] # TODO: remove nesting so many objects (there is the default XXX and body and then main div)
 
     def saveToFile(self):
         with open('test.json', mode='w', encoding='utf8') as f:
             f.write(json.dumps(self.json, indent=4, ensure_ascii=False))
-
-    def addAds(self):
-        temp_soup: BeautifulSoup = BeautifulSoup()
-        amount_of_paragraphs = len(self.soup.body.contents)
-        counter = 1
-        for ads_number in range(1, amount_of_paragraphs , 4 ):
-            t_name = 'ad-component'
-            attrs  = { 'uid': counter }
-            if counter % 2 == 0:
-                t_name = 'taboola-component'
-                attrs = {'category': "article", 'mode': "rbox-only-video", 'placement': "rbox-only-video", 'uid': counter}
-            new_tag = temp_soup.new_tag(name=t_name, attrs=attrs)
-            insert_position = ads_number + counter
-            self.soup.body.contents.insert(insert_position, new_tag)
-            counter += 1
-
-    def getImggeAlt(self, tag):
-        if (tag.contents[0]) == element.NavigableString:
-            return str(tag.contents[0].string)
-        return str(tag.text)
-        
-
 
 
 def pushToArray(elements, content :list= []):
@@ -224,29 +113,7 @@ def printTypesRecursive(obj):
         print(type(obj))
         return type(obj)
 
-def handleFlashes(flashes):
-    for index, flash in enumerate(flashes['Items']):
-        html = HtmlConverter()
-        if flash['content']:
-            html.parseHtmlContent(flash['content'], ads=False)
-            res = pushToArray(html.soup, [{TAG: 'XXX', CONTENT: [] }])[0][CONTENT]
-            flashes['Items'][index][CONTENT] = res
-    return flashes
 
-
-if __name__ == "__main__":
-    html = HtmlConverter() #int(497178) 495011 494979 471064 496027 465835 495190 466498 468418 497178 471884
-    html.initWithArticle(503161)
-    html.soupToJson()
-    import json
-    printTypesRecursive(html.respond['Content2'])
-    with open('test.json', mode='w') as f:
-        f.write(json.dumps(
-            {
-                'html': html.html,
-                'json': html.respond['Content2']
-            }
-            , indent=4, ensure_ascii=False))
 
     
     
